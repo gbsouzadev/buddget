856ab1af39738274f03fc439b531b7cf7f772892
Gabriel Souza <gbsouza.dev@gmail.com>
2023-06-29 18:05:37 -0300

    remove postgresql and add mysql dependencie, configure db connection with planetscale.com in application.properties, create user entitie and repository for ORM

---
+++ src/main/java/com/buddget/entities/User.java
@@ -0,0 +1,29 @@
+package com.buddget.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private Instant dateCreated;
+
+    private Instant lastLogin;
+}
