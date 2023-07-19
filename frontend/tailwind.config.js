27aa27d5fa2793aa0cbd0b8a31b940571d4ce2dc
Fillipi Pelz <fillipi.pelz@gmail.com>
2023-07-19 22:58:25 +0200

    install tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: ['./src/**/*.{html,js,tsx,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
